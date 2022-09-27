fce35eb0dca0dabb1b20aec39d9d9f932d71369b
Michelle <michelle.douma@gmail.com>
2022-09-27 10:53:34 -0300

    controllers created

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Comment, Vote } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+//creates a post requiring a title and body, and then takes the user.id from the session
+router.post('/', withAuth, (req, res) => {
+    console.log(req.body);
+
+    Post.create({
+      title: req.body.title,
+      body: req.body.postBody,
+      user_id: req.session.user_id,
+      
+    })
+      .then(dbPostData => res.json(dbPostData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+  //updates a post, requires a title and body
+  router.put('/:id', withAuth, (req, res) => {
+    Post.update(
+      {
+        title: req.body.title,
+        body: req.body.postBody
+
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+
+  //deletes a post where the id matches
+  router.delete('/:id', withAuth, (req, res) => {
+    console.log('id', req.params.id);
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post has been found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+  module.exports = router; 
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,75 @@
+const router = require('express').Router();
+const { User, Post, Comment, Vote } = require('../../models');
+
+
+//login page, where it will search for a user based on the given username and password
+// it will then create a new session for the user
+router.post('/login', (req, res) => {
+    
+    User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'No user found with that email address!' });
+        return;
+      }
+  
+      const validPassword = dbUserData.checkPassword(req.body.password);
+  
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+    
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+      });
+    });
+  });
+  
+
+  //logging the user out and will delete the session
+  router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    }
+    else {
+      res.status(404).end();
+    }
+  });
+
+
+  //Signing up to the site, requires a username, email and password.
+  // then will save the session for the user
+  router.post('/signup', (req, res) => {
+    
+    User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password
+    })
+      .then(dbUserData => {
+        req.session.save(() => {
+          req.session.user_id = dbUserData.id;
+          req.session.username = dbUserData.username;
+          req.session.loggedIn = true;
+    
+          res.json(dbUserData);
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  
+  
+  module.exports = router;
