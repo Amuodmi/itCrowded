3f04b626df47d8301085945c86fef565b6911b8b
Michelle <michelle.douma@gmail.com>
2022-09-27 10:59:37 -0300

    added JS logic

---
+++ public/js/edit-post.js
@@ -0,0 +1,32 @@
+//This function will take the value of the title input, and the body input, 
+//and then send a PUT request to update the post with title and body
+
+async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="edit-title"]').value.trim();
+    const postBody = document.querySelector('input[name="edit-body"]').value.trim();
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+    console.log(title, postBody);
+    const response = await fetch(`/api/posts/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+        title,
+        postBody
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.edit-post-form').addEventListener('submit', editFormHandler);
+  
---
+++ public/js/new-post.js
@@ -0,0 +1,29 @@
+// this function will add a new post. It takes the values from the title and body,
+// then sends a POST request to add the new post and replaces the location with the dashboard
+
+async function newPostHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const postBody = document.querySelector('input[name="post-body"]').value;
+  
+    console.log(title, postBody);
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        postBody
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+
+  document.querySelector('#new-post-button').addEventListener('submit', newPostHandler);
+  
